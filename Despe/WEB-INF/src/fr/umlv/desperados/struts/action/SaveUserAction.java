59fd4c235dfe9e0f51f3f7ac6a238304fb6da374
ncuvelie <ncuvelie>
2004-01-31 16:29:58 +0000

    ajout de la gestion de suppression ajout de l'envoi de mail

--- Despe/WEB-INF/src/fr/umlv/desperados/struts/action/SaveUserAction.java
+++ Despe/WEB-INF/src/fr/umlv/desperados/struts/action/SaveUserAction.java
@@ -18,9 +18,15 @@ import fr.umlv.desperados.account.User;
 import fr.umlv.desperados.account.UserAlreadyExistsException;
 import fr.umlv.desperados.account.UserManager;
 import fr.umlv.desperados.account.UserNotFoundException;
+import fr.umlv.desperados.account.UserUtilities;
+import fr.umlv.desperados.mail.MailNotSentException;
+import fr.umlv.desperados.mail.Mailer;
+import fr.umlv.desperados.mail.Message;
+import fr.umlv.desperados.mail.MessageFactory;
 import fr.umlv.desperados.struts.form.UserForm;
 import fr.umlv.desperados.struts.util.FormUtilities;
 import fr.umlv.desperados.util.Constants;
+import fr.umlv.desperados.util.ManagerException;
 
 /** 
  * SaveUserAction.java created by EasyStruts - XsltGen.
@@ -50,19 +56,12 @@ public class SaveUserAction extends AdminAction {
 		HttpServletResponse response)
 		throws Exception {
 
+		ActionErrors errors = new ActionErrors();
 		UserForm userForm = (UserForm) form;
-		HttpSession session = request.getSession();
 
 		String action = userForm.getAction();
-		if (action == null) {
-			action = "create";
-		}
-
-		UserManager manager = (UserManager)servlet.getServletContext().
-								getAttribute(Constants.USER_DATABASE_KEY);
 		if (log.isDebugEnabled()) {
-			log.debug("SaveRegistrationAction:  Processing " + action +
-					  " action");
+			log.debug("SaveRegistrationAction:  Processing " + action + " action");
 		}
 
 		// Was this transaction cancelled?
@@ -71,11 +70,10 @@ public class SaveUserAction extends AdminAction {
 				log.trace(" Transaction '" + action +
 						  "' was cancelled");
 			}
-			return (mapping.findForward("success"));
+			return (mapping.findForward("userhome"));
 		}
 
 		// Validate the transactional control token
-		ActionErrors errors = new ActionErrors();
 		if (log.isTraceEnabled()) {
 			log.trace(" Checking transactional control token");
 		}
@@ -85,23 +83,35 @@ public class SaveUserAction extends AdminAction {
 		}
 		resetToken(request);
 
-		// Update the user's persistent profile information
-		User userToSave = new User(userForm.getLogin());
-		FormUtilities.userFormToUser(userForm, userToSave);
+		// Report any errors we have discovered
+		if (!errors.isEmpty()) {
+			saveErrors(request, errors);
+			saveToken(request);
+			return (mapping.findForward("error"));
+		}
+
+
+		UserManager manager = (UserManager)servlet.getServletContext().
+								getAttribute(Constants.USER_DATABASE_KEY);
 
 		String err = null;
 		try {
-			if("create".equals(action))
-				manager.addUser(userToSave);
-			else
-				manager.modifyUser(userToSave);
+			if("delete".equals(action)) {
+				doDeleteUser(manager, userForm);
+			} else if("edit".equals(action)) {
+				doEditUser(manager, userForm);
+			} else {
+				doCreateUser(manager, userForm);
+			}
 		} catch(UserAlreadyExistsException e) {
 				err = "error.user.alreadyexist";
 		} catch(UserNotFoundException e) {
 			err = "error.user.dontexist";
+		} catch(ManagerException e) {
+			err = e.getMessage();
 		}
 		if(err != null) {
-			errors.add("database",
+			errors.add(ActionErrors.GLOBAL_ERROR,
 					   new ActionError(err));
 		}
 
@@ -109,8 +119,10 @@ public class SaveUserAction extends AdminAction {
 		if (mapping.getAttribute() != null) {
 			if ("request".equals(mapping.getScope()))
 				request.removeAttribute(mapping.getAttribute());
-			else
+			else {
+				HttpSession session = request.getSession();
 				session.removeAttribute(mapping.getAttribute());
+			}
 		}
 
 		// Report any errors we have discovered back to the original form
@@ -127,4 +139,75 @@ public class SaveUserAction extends AdminAction {
 		return (mapping.findForward("success"));
 	}
 
+
+	private void doCreateUser(UserManager manager, UserForm form)
+	throws UserAlreadyExistsException, ManagerException, MailNotSentException {
+
+		User userToSave = new User();
+		FormUtilities.userFormToUser(form, userToSave);
+
+		// generate the login
+		String login = UserUtilities.generateLogin(userToSave.getName(),
+																					userToSave.getFirstname());
+		int i = 1;
+		while(manager.existUser(login)) {
+			UserUtilities.generateLogin(userToSave.getName(),
+																userToSave.getFirstname(),
+																i);
+			i++;
+		}
+		userToSave.setLogin(login);
+
+		// generate randomly a new password
+		userToSave.setPassword(UserUtilities.generatePassword());
+
+		// add the User to the database
+		manager.addUser(userToSave);
+
+		mail("create", userToSave);
+	}
+
+
+	private void doDeleteUser(UserManager manager, UserForm form)
+		throws UserNotFoundException, ManagerException, MailNotSentException {
+
+		User userDeleted = manager.removeUser(form.getLogin());
+
+		mail("delete", userDeleted);
+	}
+
+
+	private void doEditUser(UserManager manager, UserForm form)
+		throws UserNotFoundException, ManagerException, MailNotSentException {
+
+		User userToSave = manager.getUser(form.getLogin());
+		FormUtilities.userFormToUser(form, userToSave);
+
+		// generate randomly a new password
+		if(form.getGeneratePassword()) {
+			userToSave.setPassword(UserUtilities.generatePassword());
+		}
+
+		// update the User in the database
+		manager.modifyUser(userToSave);
+
+		mail("modify", userToSave);
+	}
+
+	private void mail(String action, User user) throws MailNotSentException {
+		MessageFactory factory = new MessageFactory();
+		Mailer mailer = new Mailer();
+		Message message = null;
+		if("create".equals(action)) {
+			message = factory.createMessage(Message.CREATION_MESSAGE,
+																			user);
+		} else if("delete".equals(action)) {
+			message = factory.createMessage(Message.SUPPRESSION_MESSAGE,
+																			user);
+		} else {
+			message = factory.createMessage(Message.MODIFICATION_MESSAGE,
+																			user);
+		}
+		mailer.sendMail(user.getEmail(), message);
+	}
 }
