35eadf5b505e311d36f637e7f2e8719f3151976d
ndedanil <ndedanil>
2004-02-21 10:07:45 +0000

    *** empty log message ***

--- Despe/WEB-INF/src/fr/umlv/desperados/stylesheet/junit/DatabaseStyleSheetManagerTest.java
+++ Despe/WEB-INF/src/fr/umlv/desperados/stylesheet/junit/DatabaseStyleSheetManagerTest.java
@@ -11,12 +11,15 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 import fr.umlv.desperados.database.DatabaseRequestor;
 import fr.umlv.desperados.struts.database.StrutsDatabaseRequestor;
 import fr.umlv.desperados.stylesheet.DatabaseStyleSheetManager;
 import fr.umlv.desperados.stylesheet.ExistStylesheetException;
 import fr.umlv.desperados.stylesheet.StyleSheet;
+import fr.umlv.desperados.stylesheet.UsedStylesheetException;
 
 import junit.framework.TestCase;
 
@@ -70,19 +73,56 @@ public class DatabaseStyleSheetManagerTest extends TestCase {
 		dssm = null;
 	}
 
-	public void testAddStyleSheet() {
-		StyleSheet ss = new StyleSheet("dossier5.xsl","theDossierFile5");
-		// dssm.addStyleSheet(ss);
-		try {
-			ResultSet rs = requestor.doQuery("select * from FEUILLE_DE_STYLE where NOM_FEU = 'theDossierFile5'");
-			assertTrue(rs.getString("NOM_FIC_FEU").equals("dossier5.xsl"));
-		} catch (SQLException e) {
-			// TODO Bloc catch auto-généré
-			e.printStackTrace();
-		}
+	public void testAddStyleSheet() throws ExistStylesheetException, SQLException {
+		StyleSheet ss = new StyleSheet("feuille_test.xsl","test");
+		dssm.addStyleSheet(ss);
+		System.out.println("ss = "+ss.getFilename());
+		ResultSet rs = requestor.doQuery("select * from FEUILLE_DE_STYLE where NOM_FIC_FEU = '" + ss.getFilename()+"'");
+		assertTrue(rs.first());
+		assertTrue(rs.getString("NOM_FIC_FEU").equals("feuille_test.xsl"));
 	}
 	
 	public void testRemoveStyleSheet() {
-		//dssm.removeStyleSheet();
+		try {
+			dssm.removeStyleSheet("feuille_test.xsl","/stylesheet/");
+		} catch (UsedStylesheetException e) {
+			e.printStackTrace();
+		}
+		try {
+			ResultSet rs = requestor.doQuery("select * from FEUILLE_DE_STYLE where NOM_FIC_FEU='feuille_test.xsl'");
+			assertTrue(!rs.first());
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	public void testSetStyleSheet() throws ExistStylesheetException, SQLException {
+		StyleSheet ss = new StyleSheet("feuille_test.xsl","test");
+		dssm.addStyleSheet(ss);
+		dssm.setStyleSheet(ss.getFilename(),DatabaseStyleSheetManager.CONFIRMATION_DOCUMENT);
+		ResultSet rs = requestor.doQuery("select  FEUILLE_STYLE_DOC from DOCUMENT where ID_DOC="+DatabaseStyleSheetManager.CONFIRMATION_DOCUMENT);
+		assertTrue(rs.first());
+		assertTrue(rs.getString("FEUILLE_STYLE_DOC").equals("feuille_test.xsl"));
+	}
+	
+	public void testListStyleSheet() {
+		List l = dssm.listStyleSheet();
+		System.out.println(l.size());
+		assertTrue(l.size() > 0);
+	}
+	
+	public void testGetStyleSheet() {
+		StyleSheet ss = dssm.getStyleSheet(DatabaseStyleSheetManager.CONFIRMATION_DOCUMENT);
+		assertTrue(ss.getFilename().equals("feuille_test.xsl"));
+		assertTrue(ss.getName().equals("test"));
+	}
+	
+	public void testListDocType() throws UsedStylesheetException {
+		Map map = dssm.listDocType();
+		assertTrue(!map.isEmpty());
+		assertTrue(map.containsValue("inscription"));
+		assertTrue(map.containsValue("confirmation"));
+		dssm.setStyleSheet("confirmation.sxw", DatabaseStyleSheetManager.CONFIRMATION_DOCUMENT);
+		dssm.removeStyleSheet("feuille_test.xsl","/stylesheet");
 	}
 }
